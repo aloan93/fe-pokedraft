1bb4d9d9cb1f46843ae38a27addee76bfecb73c3
aloan93 <a.loan93@gmail.com>
2024-02-20 21:35:21 +0000

    implemented a search bar component designed to be modular - current working as filter by ability in all pokemon

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -3,6 +3,7 @@ import pokedraftAPI from "../api/api";
 import PageNav from "./PageNav";
 import Order from "./Order";
 import SortBy from "./SortBy";
+import SearchBar from "./SearchBar";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
@@ -12,12 +13,17 @@ export default function PokemonList() {
   const [page, setPage] = useState(1);
   const [order, setOrder] = useState("asc");
   const [sortBy, setSortBy] = useState("pokedex_no");
+  const [ability, setAbility] = useState(null);
 
   useEffect(() => {
     setIsLoading(true);
     setError(null);
     pokedraftAPI
-      .get(`/pokemon?page=${page}&order=${order}&sort_by=${sortBy}`)
+      .get(
+        `/pokemon?page=${page}&order=${order}&sort_by=${sortBy}${
+          ability ? `&ability=${ability}` : ``
+        }`
+      )
       .then(({ data: { total, pokemon } }) => {
         setIsLoading(false);
         setPokemon(pokemon);
@@ -27,12 +33,17 @@ export default function PokemonList() {
         setIsLoading(false);
         setError("Something went wrong");
       });
-  }, [page, order, sortBy]);
+  }, [page, order, sortBy, ability]);
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
   return (
     <>
+      <SearchBar
+        criteria="Ability"
+        setCriteria={setAbility}
+        setPage={setPage}
+      />
       <SortBy sortBy={sortBy} setSortBy={setSortBy} setPage={setPage} />
       <Order order={order} setOrder={setOrder} setPage={setPage} />
       <ul>
