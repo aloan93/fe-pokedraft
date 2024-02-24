841e47926f4c563df1151e7a5574ef602d256f55
aloan93 <a.loan93@gmail.com>
2024-02-24 16:29:57 +0000

    implemented signup page for adding new users - redirect to new profile in place once successful

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,78 @@
+import { useState, useEffect, useContext } from "react";
+import { UserContext } from "../contexts/User";
+import { pokedraftAPI } from "../api/api";
+import { useNavigate } from "react-router-dom";
+
+export default function SignUp() {
+  const { user, setUser } = useContext(UserContext);
+  const [input, setInput] = useState({
+    username: "",
+    email: "",
+    password: "",
+    retypePassword: "",
+  });
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) navigate("/profile");
+  }, [user]);
+
+  function signUpAttempt(e) {
+    e.preventDefault();
+    if (input.password !== input.retypePassword)
+      setError("Passwords does not match");
+    else {
+      setIsLoading(true);
+      setError(null);
+      pokedraftAPI
+        .post("/users", {
+          username: input.username,
+          password: input.password,
+          email: input.email,
+        })
+        .then(({ data: { user } }) => {
+          setUser(user);
+          setIsLoading(false);
+        })
+        .catch(
+          ({
+            response: {
+              data: { message },
+            },
+          }) => {
+            setIsLoading(false);
+            setError(message);
+          }
+        );
+    }
+  }
+
+  function handleInput(e) {
+    const { id, value } = e.target;
+    setInput((input) => ({
+      ...input,
+      [id]: value,
+    }));
+  }
+
+  if (isLoading) return <p>Loading...</p>;
+  else
+    return (
+      <>
+        <form onSubmit={signUpAttempt}>
+          <label htmlFor="username">Username:</label>
+          <input id="username" type="text" onChange={handleInput} />
+          <label htmlFor="email">Email:</label>
+          <input id="email" type="email" onChange={handleInput} />
+          <label htmlFor="password">Password:</label>
+          <input id="password" type="password" onChange={handleInput} />
+          <label htmlFor="retypePassword">Retype Password:</label>
+          <input id="retypePassword" type="password" onChange={handleInput} />
+          <button>Sign-Up</button>
+        </form>
+        {error ? <p>{error}</p> : null}
+      </>
+    );
+}
