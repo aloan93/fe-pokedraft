28199e7f877ebe693e15d9de08a1c32191ea64f9
aloan93 <a.loan93@gmail.com>
2024-03-09 01:46:26 +0000

    further styling to pokemon list cards etc

--- src/components/PokemonListCard.jsx
+++ src/components/PokemonListCard.jsx
@@ -4,21 +4,25 @@ import { capitalLetter } from "../../utilities/utils";
 export default function PokemonListCard({ pokemon }) {
   return (
     <li key={pokemon.pokemon_name}>
-      <div className="pokemonListCardDiv">
-        <Link to={`/pokemon/${pokemon.pokemon_name}`}>
-          {`#${pokemon.pokedex_no} ${capitalLetter(pokemon.pokemon_name)}`}
-        </Link>
-        <div className="typeDiv">
-          <p className="typeTag" id="type1">
-            {capitalLetter(pokemon.type_1)}
-          </p>
-          {pokemon?.type_2 ? (
-            <p className="typeTag" id="type2">
-              {capitalLetter(pokemon.type_2)}
+      <Link to={`/pokemon/${pokemon.pokemon_name}`}>
+        <div className="pokemonListCardDiv">
+          <p className="pokemonNumName">{`#${
+            pokemon.pokedex_no
+          } ${capitalLetter(pokemon.pokemon_name)}`}</p>
+          <div className="typeDiv">
+            <p className="type1Tag" id={`type${capitalLetter(pokemon.type_1)}`}>
+              {capitalLetter(pokemon.type_1)}
             </p>
-          ) : null}
+            {pokemon?.type_2 ? (
+              <p
+                className="type2Tag"
+                id={`type${capitalLetter(pokemon.type_2)}`}>
+                {capitalLetter(pokemon.type_2)}
+              </p>
+            ) : null}
+          </div>
         </div>
-      </div>
+      </Link>
     </li>
   );
 }
