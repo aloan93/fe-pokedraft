d29c2a60bb35af19d2c69c748e5f5a7d52f1cb53
aloan93 <a.loan93@gmail.com>
2024-04-21 14:35:39 +0100

    removed obsolete ternary from ShownResults

--- src/components/ShownResults.jsx
+++ src/components/ShownResults.jsx
@@ -1,7 +1,7 @@
 export default function ShownResults({ resultTotal, page, isInvalidPage }) {
-  const currentResultsShown = `Showing results ${
-    resultTotal === 0 ? 0 : page * 20 - 19
-  } - ${page * 20 > resultTotal ? resultTotal : page * 20}`;
+  const currentResultsShown = `Showing results ${page * 20 - 19} - ${
+    page * 20 > resultTotal ? resultTotal : page * 20
+  }`;
 
   if (resultTotal === 0)
     return <p className="shownResults">No matching results</p>;
