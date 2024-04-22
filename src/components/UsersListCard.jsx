60922e272724750f8492eaf3e5edeb6f9ff91304
aloan93 <a.loan93@gmail.com>
2024-04-22 23:07:35 +0100

    new util function implemented to convert date iso strings to YYYY-MM-DD format - put to use on userslistcards and profile components

--- src/components/UsersListCard.jsx
+++ src/components/UsersListCard.jsx
@@ -1,10 +1,13 @@
-// import { Link } from "react-router-dom";
+import { convertISOToDate } from "../../utilities/utils";
 
 export default function UsersListCard({ user }) {
   return (
     <div className="usersListCardDiv">
-      <p>{user.username}</p>
+      <p className="usersListCardUsername">{user.username}</p>
       <img src={user.avatar_url} alt={`Profile picture for ${user.username}`} />
+      <p className="usersListCardJoinDate">{`Joined: ${convertISOToDate(
+        user.join_date
+      )}`}</p>
     </div>
   );
 }
